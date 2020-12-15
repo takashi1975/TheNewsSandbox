75f9a276d5fcadd0b534c0f4c1e7b562f170335e
ohsaka <takashi.ohsaka@gmail.com>
2020-12-15 10:57:29 +0900

    新規作成

---
+++ TheNewsSandbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheNewsSandbox
+//
+//  Created by MacBookAir on 2020/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
